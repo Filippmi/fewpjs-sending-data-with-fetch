eeebb8689ef5415cb37d678fd3d2576ce4b8d321
Filipp Minayev <Filippmi@gmail.com>
2021-02-24 08:13:11 -0700

    commit tree 558093f1d4065e3fa3a489024877f548b9a44e23

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// let submitData = {
+//   name: "Filipp",
+//   email: "Filipp@gmail.com"
+// }
+
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(submitData)
+// }
+
+// fetch("http://localhost:3000/users", configObj)
+//   .then(function(resp) {
+//     return resp.json()
+//   })
+//   .then(function(obj) {
+//     console.log(obj)
+//   })
+//   .catch(function(error) {
+//     alert("Bad things!")
+//     console.log(error.message)
+//   })
+  function submitData( name, email ) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(function(resp) {
+      return resp.json()
+    })
+    .then(function(obj) {
+      document.body.innerHTML = obj[ "id" ]
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+  }
